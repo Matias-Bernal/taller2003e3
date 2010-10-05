8803d6e55b292b8710577fe133ab525480daf739
Eduardo.Odorizzi <Eduardo.Odorizzi@e71d9da0-65db-fefe-025e-11901647b32e>
2010-10-05 14:53:50 +0000

    Commit inicial con los perfiles de la función

---
+++ src/java/Calculadora.java
@@ -0,0 +1,30 @@
+package java;
+
+public class Calculadora {
+ 
+	public Polinomio suma(Polinomio p, Polinomio q) {
+		return null;
+	}
+	 
+	public Polinomio resta(Polinomio p, Polinomio q) {
+		return null;
+	}
+	 
+	public Polinomio producto(Polinomio p, Polinomio q) {
+		return null;
+	}
+	 
+	public Polinomio cociente(Polinomio p, Polinomio q) {
+		return null;
+	}
+	 
+	public Polinomio cocienteRuffini(Polinomio p, Polinomio q) {
+		return null;
+	}
+	 
+	public boolean esPosibleRuffini(Polinomio p, Polinomio q) {
+		return true;
+	}
+	 
+}
+ 
---
+++ src/java/Termino.java
@@ -0,0 +1,26 @@
+package java;
+
+public class Termino {
+ 
+	private int valor;
+	 
+	private int exponente;
+	 
+	public int getValor() {
+		return 0;
+	}
+	 
+	public void setValor(int v) {
+	 
+	}
+	 
+	public int getExponente() {
+		return 0;
+	}
+	 
+	public void setExponente(int e) {
+	 
+	}
+	 
+}
+ 
