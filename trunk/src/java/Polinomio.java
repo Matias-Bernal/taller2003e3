6c7c6b1986572e6f7ccc4ac2f78a455c6613d737
Eduardo.Odorizzi <Eduardo.Odorizzi@e71d9da0-65db-fefe-025e-11901647b32e>
2010-10-05 14:53:50 +0000

    Commit inicial con los perfiles de la función

---
+++ trunk/src/java/Polinomio.java
@@ -0,0 +1,38 @@
+package java;
+
+import java.util.Vector;
+
+public class Polinomio {
+ 
+	private Vector terminos;
+	 
+	public void makePolinomioFromFile() {
+	 
+	}
+	 
+	public void verPolinomio() {
+	 
+	}
+	 
+	public void addTermPolinomio(int v, int e) {
+	 
+	}
+	 
+	public Polinomio ordenarDec(Polinomio p) {
+		return null;
+	}
+	 
+	public Polinomio completar(Polinomio p) {
+		return null;
+	}
+	 
+	public Polinomio estaordenado(Polinomio p) {
+		return null;
+	}
+	 
+	public int grado(Polinomio p) {
+		return 0;
+	}
+	 
+}
+ 
